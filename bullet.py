8c6bbde73beec02a435b864c83c90e0be2b6e75f
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-01-21 16:46:41 +0200

    vrewr

--- bullet.py
+++ bullet.py
@@ -1,7 +1,5 @@
 import pygame
 
-
-
 class bullet:
     def __init__(self, x, y,w,h, speed, texture):
         self.speed = speed
@@ -11,17 +9,15 @@ class bullet:
         self.hit_box.x = x
         self.hit_box.y = y
         self.texture = pygame.transform.scale(self.texture,(w,h))
-
     def render(self, window):
-        pygame.draw.rect(window, (255, 0, 0), self.hit_box)
-
+        pygame.draw.rect(window, (255, 0, 0), self.hit_box)<<<<<<< HEAD
         window.blit(self.texture, (self.hit_box.x, self.hit_box.y))
 
+    def muve(self):
+        self.hit_box.y -= self.speed
+        window.blit(self.texture, (self.hit_box.x, self.hit_box.y))
 
     def muve(self):
         self.hit_box.y += self.speed
-
         if self.hit_box.y > 500:
             self.hit_box.y = -50
-
-
--- main.py
+++ main.py
@@ -1,59 +1,77 @@
 import pygame
+import time
+import pygame
 import shoot
 import ufoner
 import bullet
 
 
+pygame.init()
 window = pygame.display.set_mode((800,500))
-
-
 
 fone = pygame.image.load("1626767990_23-kartinkin-com-p-galakticheskii-fon-krasivo-36.jpg")
 
-
-
 fps = pygame.time.Clock()
-
-
+n = 0
+startTime = time.time()
+timer = int(time.time()-startTime)
+timeText = pygame.font.Font(None, 56).render("Час:" + str(timer), True,(0,0,0))
+level = 1
+leveltext = pygame.font.Font(None, 56).render("Левел:" + str("level"), True,(0,0,0))
+losetext = pygame.font.Font(None, 56).render("Ти програв:", True,(0,0,0))
+Wintext = pygame.font.Font(None, 56).render("Ти виграв :", True,(0,0,0))
+chel = shoot.shoot(48,437,50,60,1,"png-transparent-rocket-launch-spacecraft-buran-graphy-rockets-space-falcon-9-space-shuttle-thumbnail.png")
 chel = shoot.shoot(48,437,50,60,1,("rocked.png"))
 enemies = []
 emo = []
 enemies.append(ufoner.ufoner(50, 0,50,50,1,"asteroid (1).png"))
 enemies.append(ufoner.ufoner(123, 0,50,50,1,"asteroid (1).png"))
 enemies.append(ufoner.ufoner(233, 42,50,50,1,"asteroid (1).png"))
-
-
+enemies.append(ufoner.ufoner(332, 6,50,50,1,"asteroid (1).png"))
+enemies.append(ufoner.ufoner(445, 10,50,50,1,"asteroid (1).png"))
 game = True
-
-
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            print(x, y)
+        print(x,y)
         if event.type == pygame.QUIT:
             game = False
             pygame.quit()
-
     chel.muve()
-
-
-    for enemy in enemies:
-        enemy.muve()
+    if len(enemies) == 0:
+        level+= 1
+        leveltext = pygame.font.Font(None, 56).render("Левел:" + str(level), True, (0, 0, 0))
+        timer = int(time.time() - startTime)
+    timeText = pygame.font.Font(None, 56).render("Час:" + str(timer), True, (0, 0, 0))
+    for enemy in enemies:        enemy.muve()
     window.fill((123,123,123))
-
     for enemy in enemies:
         if enemy.hit_box.colliderect((chel.hit_box)):
             game = False
 
-        if enemy.hit_box.colliderect((chel.hit_box)):
-            game = False
 
     window.blit(fone, (0, 0))
     chel.render(window)
-
+    for i in range(len(enemies)):
+        enemies[i].render(window)
+        window.blit(timeText, [0, 0])
+    window.blit(leveltext, [150, 0])
     for enemy in enemies:
         enemy.render(window)
-    pygame.display.flip()
-    fps.tick()
+    for enemy in enemies:
+
+        for p in chel.bullets:
+
+            for i in range(len(enemies)):
+                if enemies[i].rect.y > 500:
+                    window.fill((255, 0, 0))
+            window.blit(losetext, [100, 100])
+            if timer > 60:
+
+window.fill((0, 255, 0))
+window.blit(Wintext, [100, 100])
+game = False
+pygame.display.flip()
+fps.tick()
